92d09dd6aa8abea41c8f6f7317afc245a2694fa9
BrunoBarraza <bruno.barraza@atos.net>
2022-07-07 08:04:50 -0500

    update

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,24 @@
-fetch('https://restcountries.com/v3.1/all').then((response) =>{
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch((err) => {
-    console.log('rejected', err);
-});
+
+fetch('https://restcountries.com/v3.1/all')
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (countries) {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+        for (let country of countries) {
+            out += `
+            <tr>
+                <td>${country.name.official}</td>
+                <td>${country.capital}</td>
+                <td>${country.region}</td>
+                <td>${country.languages}</td>
+                <td>${country.population}</td>
+                <td><img src='${country.flags.png}'></img></td>
+            </tr>
+            `;
+        }
+
+        placeholder.innerHTML = out;
+    })
